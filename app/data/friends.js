415555ac1971bdc699f9f794d66bcd69a3c5982a
klwidmer <klwidmer@gmail.com>
2018-05-13 09:03:28 -0500

    FullStackApp

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {   name: "Kevin",
+        photo: "lfksj;dl",
+        score: [1,2,3,2,4]
+    },
+    {   name: "Brittney",
+        photo: "lfksj;dl",
+        score: [1,2,1,2,3]
+    },
+    {   name: "Everett",
+        photo: "lfksj;dl",
+        score: [2,5,5,2,1]
+    },
+    {   name: "Audrey",
+        photo: "lfksj;dl",
+        score: [1,1,3,2,2]
+    },
+    {   name: "Suki",
+        photo: "lfksj;dl",
+        score: [1,1,3,3,3]
+    },
+    {   name: "Mowglie",
+        photo: "lfksj;dl",
+        score: [4,4,4,4,4]
+    }
+];
